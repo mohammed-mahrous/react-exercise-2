e2df2f88c4dbaa252c4a611ce0f849ee87be3774
mohammed-mahrous <xxspider6@gmail.com>
2021-12-10 16:02:21 +0200

    solved

--- src/App.js
+++ src/App.js
@@ -2,13 +2,6 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-// const value1 = Math.floor(Math.random() * 100);
-// const value2 = Math.floor(Math.random() * 100);
-// const value3 = Math.floor(Math.random() * 100);
-// const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-// const numQuestions = 0;
-// const numCorrect = 0;
-
 class App extends Component {
   state = {
     value1: Math.floor(Math.random() * 100),
